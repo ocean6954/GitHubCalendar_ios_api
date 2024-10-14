6a65a485c851d74ae7bcf8a4a16cd7bdd7b58e2e
ocean6954 <oceanan699@gmail.com>
2024-10-14 22:12:14 +0900

    [update] コントローラの出力コード削除

--- app/controllers/api/contributions_controller.rb
+++ app/controllers/api/contributions_controller.rb
@@ -3,7 +3,6 @@ module Api
   class ContributionsController < ApplicationController
     def index
       @contributions = Contribution.all.order(date: :asc) # 日付順に全てのContributionを取得
-      puts "indexアクション"
       render json:@contributions
     end
   end
