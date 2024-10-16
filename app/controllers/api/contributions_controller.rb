cdfcfd9a7f1b48e12d47aa4603822fdb59edee8a
ocean6954 <oceanan699@gmail.com>
2024-10-17 02:55:40 +0900

    [update]コントローラアクション書いてみた

--- app/controllers/api/contributions_controller.rb
+++ app/controllers/api/contributions_controller.rb
@@ -1,15 +1,28 @@
 # app/controllers/api/contributions_controller.rb
 module Api
   class ContributionsController < ApplicationController
+
     def index
       @contributions = Contribution.all.order(date: :asc) # 日付順に全てのContributionを取得
       render json:@contributions
     end
     def create
+      @contirbution = Contirbution.new(contirbution_params)
+
+      if @contirbution.save
+      render json: @contirbution, status: :created
+      else
+        render json: @contirbution.errors, status: :unprocessable_entity
+      end
     end
 
     private
+
     def contirbution_params
+      puts "受け取ったデータ"
+      puts :contirbution
+      params.require(:contirbution).permit(:date, :contribution_count)
     end
+
   end
 end
