a0f0194e93b3c96f1ab535ce80d1fbd49bea3fe1
ocean6954 <oceanan699@gmail.com>
2024-11-04 23:43:39 +0900

    [update]contributionモデルのカラムの追加

--- db/migrate/20241028144304_create_weeks.rb
+++ db/migrate/20241028144304_create_weeks.rb
@@ -1,7 +1,6 @@
 class CreateWeeks < ActiveRecord::Migration[7.1]
   def change
     create_table :weeks do |t|
-      t.integer :day_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_11_182406) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_01_185110) do
   create_table "contributions", force: :cascade do |t|
     t.date "date"
     t.integer "contribution_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "week_id"
+  end
+
+  create_table "weeks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
